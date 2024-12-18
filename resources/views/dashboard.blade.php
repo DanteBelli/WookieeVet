52c12474e1462e3d7baf440e428a828b2dbfe59c
DanteBell95 <dante.belli18@gmail.com>
2024-12-18 20:58:07 -0300

    Modificaciones admin

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -11,6 +11,10 @@
             @switch($rolId)
                 @case(1)
                 <a href="{{ route('administrador.mainAdm') }}" class="inline-block mt-4 bg-blue-500 text-white px-4 py-2 rounded">Editar Usuarios</a>
+                <a href="{{ route('mascotas.create') }}" class="inline-block mt-4 bg-blue-500 text-white px-4 py-2 rounded">Agregar Mascota</a>
+                <a href="{{ route('mascotas.edit') }}" class="inline-block mt-4 bg-blue-500 text-white px-4 py-2 rounded">Editar Mascotas</a>
+                <a href="{{ route('observacion.create')}}" class="inline-block mt-4 bg-blue-500 text-white px-4 py-2 rounded">Agregar Observacion</a>
+                <a href="{{ route('observacione.select') }}" class="inline-block mt-4 bg-blue-500 text-white px-4 py-2 rounded">Consultar Observaciones</a>
                 @break
                 @case(2)
                     <a href="{{ route('mascotas.create') }}" class="inline-block mt-4 bg-blue-500 text-white px-4 py-2 rounded">Agregar Mascota
